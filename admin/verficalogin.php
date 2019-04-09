635fc92f43e9abfd88075f7c21b5b471dd8cd198
matheus <sr.jup1t3r@yahoo.com>
2019-04-09 12:40:54 -0300

    first commit

---
+++ admin/verficalogin.php
@@ -0,0 +1,23 @@
+<? 
+
+require("../config/config.php");
+
+$sql ="SELEC email,senha FROM funcionarios WHERE
+	email= ".$usuario."'AND senha = '".$senha."'";
+
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
+
+$quer
+
+if($usuario =="Matheus"){
+
+
+}
+
+
+
+
+
+
+?>
